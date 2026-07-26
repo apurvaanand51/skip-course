a24b101f3aaf45c11160074f0dc9f9a1d009e159
Apurva Anand <apurvaanand51@gmail.com>
2026-07-26 18:00:05 +0530

    toogle theme

--- web/main.js
+++ web/main.js
@@ -19,6 +19,34 @@ const extractBtn = document.getElementById("extract");
 const copySlugBtn = document.getElementById("copy-slug");
 
 
+// --------------------
+// Theme Toggle
+// --------------------
+
+const themeToggle = document.getElementById("theme-toggle");
+
+const savedTheme = localStorage.getItem("theme");
+
+if(savedTheme === "dark"){
+    document.body.classList.add("dark");
+    themeToggle.textContent = "Light Mode";
+}
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme","dark");
+        themeToggle.textContent = "Light Mode";
+    }else{
+        localStorage.setItem("theme","light");
+        themeToggle.textContent = "Dark Mode";
+    }
+
+});
+
+
 
 // --------------------
 // Helper
